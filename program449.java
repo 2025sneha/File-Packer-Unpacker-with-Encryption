adf46980aefd732c94e941a20f006916a1a31a49
Sneha Mothabhau Mohane <sneha.mohane1604@gmail.com>
2026-01-23 10:50:50 +0530

    Add files via upload

---
+++ program449.java
@@ -0,0 +1,57 @@
+
+/////////////////////////////////////////////////////////////////////
+//
+//  Header files inclusion
+//
+///////////////////////////////////////////////////////////////////////
+
+import java.io.*;
+import java.util.*;
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	 Class Name :       program449.java
+//	 Description :	    Used to accept name from user and open folder and read the bytes of data 
+//                      of all files and display it on screen using file handling 
+//   Author :           Sneha Mohane
+//   Date :             20/07/2025                 
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+class program449
+{
+    public static void main(String A[])
+    {
+        try
+        {
+            Scanner sobj = new Scanner(System.in);
+
+            System.out.println("Enter the name of Directory : ");
+            String DirName = sobj.nextLine();
+
+            File fobj = new File(DirName);
+
+            if(fobj.exists())
+            {
+                System.out.println("Directory is present");
+
+                File Arr[] = fobj.listFiles();
+
+                System.out.println("Number of files in the directory are : "+Arr.length);
+            
+                int i = 0;
+
+                for(i = 0; i < Arr.length; i++)
+                {
+                    System.out.println("File name is : "+Arr[i].getName()+" File size is : "+Arr[i].length());
+                }          
+            }
+            else
+            {
+                System.out.println("There is no such directory");
+            }
+        }
+        catch(Exception eobj)
+        {}
+    }
+}
